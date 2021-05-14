1c973252211c88daf9976280595acb97f7aec87c
TheRG2008 <79916337+TheRG2008@users.noreply.github.com>
2021-05-14 12:34:49 +0500

    Add Comments

--- Assets/Scripts/Scull.cs
+++ Assets/Scripts/Scull.cs
@@ -1,12 +1,10 @@
-﻿
 using UnityEngine;
 
 public class Scull : MonoBehaviour
 {
     [SerializeField] private GameObject _particalSystem;
-    public bool IsActive = false;
-    
-
+    public bool IsActive = false;    
+    // Объекты для квеста активации портала. При активации IsActive == true, включаем ParticalSystem и звук
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
@@ -16,9 +14,7 @@ public class Scull : MonoBehaviour
                 _particalSystem.SetActive(true);
                 gameObject.GetComponentInParent<AudioSource>().enabled = true;
                 IsActive = true;
-            }
-            
+            }            
         }
     }
-
 }
