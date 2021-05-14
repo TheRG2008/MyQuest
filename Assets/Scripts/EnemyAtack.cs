7fe301db6ffc739147c8b853a436873e06fff547
TheRG <TheRG2008@gmail.com>
2021-05-14 22:53:31 +0500

    Mini Change

---
+++ Assets/Scripts/EnemyAtack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TestButton : MonoBehaviour
+{
+
+    public UnityEvent AtackEvent;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            AtackEvent.Invoke();
+
+        }
+    }
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-  
+    [SerializeField] private GameObject _canvas;
 
+
+    private void PlayerIsDead()
+    {
+        _canvas.SetActive(true);
+        GetComponent<PlayerMove>().enabled = false;
+        GetComponent<Animator>().SetBool("walk", false);
+        Invoke("ReloadScene", 2);
+
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        CancelInvoke();
+    }
 
 }
