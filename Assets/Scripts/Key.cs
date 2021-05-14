6e530d17f04f4fe1e8b42921a62e8677626a00d8
TheRG2008 <79916337+TheRG2008@users.noreply.github.com>
2021-05-14 12:24:14 +0500

    Add comments

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -2,35 +2,32 @@
 using UnityEngine;
 
 public class Key : MonoBehaviour
-{
-    
+{    
     [SerializeField] private GameObject _keySound;
-    [SerializeField] private GameObject _door;
-   
+    [SerializeField] private GameObject _door;   
 
+    //Проверяем находиться ли Player в Коллайдере => возвращаем true связанной с этим ключем двери
     private void OnTriggerEnter(Collider other)
-    {
-        
+    {        
         if (other.tag == "Player")
         {
             _door.GetComponent<Door>().IsOpen = true;
             EnterAnimation();
         }
-
-    }
-
-    void StopKeySound ()
-    {
-        _keySound.GetComponent<AudioSource>().enabled = false;
-        CancelInvoke();
-
-    }
-
+    }  
+    //Активируем звук подбора ключа, уничтожаем GameObject
     void EnterAnimation ()
     {
         _keySound.GetComponent<AudioSource>().enabled = true;        
         gameObject.SetActive(false);
         Invoke("StopKeySound", 3);
+    }
+    
+    //Декактивируем звук подбора ключа, что бы сработал звук при подборе следующего ключа
+      void StopKeySound ()
+    {
+        _keySound.GetComponent<AudioSource>().enabled = false;
+        CancelInvoke();
     }
 
 }
